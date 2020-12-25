4807e15fdeabdaed51973bdc39420aa656f51702
bernco <chisombernard@gmail.com>
2020-12-25 21:51:53 +0100

    List comprehension implemented

--- main.py
+++ main.py
@@ -14,16 +14,13 @@ state_turtle = Turtle()
 state_turtle.hideturtle()
 state_turtle.pu()
 
-remaining_list = []
 answered_list = []
 # continues asking for user input until all states have been gotten
 while question.score <= question.total_states:
     answered_question = question.ask_question()
     # if the user inputs exit, create a csv file of the remaining states of which the user did not answer
     if answered_question == "Exit":
-        for states in question.STATES_READ:
-            if states not in answered_list:
-                remaining_list.append(states)
+        remaining_list = [states for states in question.STATES_READ if states not in answered_list]
         remaining_states = pandas.DataFrame(remaining_list)
         remaining_states.to_csv("remaining-states.csv")
         break
