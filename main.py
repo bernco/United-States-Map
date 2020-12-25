3099e96b78d84ad4044417ccfd515bf30c5a3252
bernco <chisombernard@gmail.com>
2020-12-25 18:46:48 +0100

    Now monitors if user inputs same state twice. Also if user can no longer answer, he/she can type in exit and get a csv file of states which were not answered.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 from question import QuestionBoard
+import pandas
 
 question = QuestionBoard()
 
@@ -13,13 +14,26 @@ state_turtle = Turtle()
 state_turtle.hideturtle()
 state_turtle.pu()
 
+remaining_list = []
+answered_list = []
 # continues asking for user input until all states have been gotten
 while question.score <= question.total_states:
     answered_question = question.ask_question()
-    if question.check_answer(answered_question):
-        question.add_score()
-        state_turtle.goto(question.set_position(answered_question))
-        state_turtle.write(answered_question)
+    # if the user inputs exit, create a csv file of the remaining states of which the user did not answer
+    if answered_question == "Exit":
+        for states in question.STATES_READ:
+            if states not in answered_list:
+                remaining_list.append(states)
+        remaining_states = pandas.DataFrame(remaining_list)
+        remaining_states.to_csv("remaining-states.csv")
+        break
+    elif question.check_answer(answered_question):
+        if answered_question not in answered_list:
+            question.add_score()
+            state_turtle.goto(question.set_position(answered_question))
+            state_turtle.write(answered_question)
+            answered_list.append(answered_question)
     else:
         question.ask_question()
-my_screen.exitonclick()
+
+# my_screen.exitonclick()
--- question.py
+++ question.py
@@ -4,9 +4,6 @@ import pandas
 # using the pandas library, read the csv and save in a file called state_files
 STATE_FILES = pandas.read_csv("50_states.csv")
 
-# saves the states extracts to list
-STATES_READ = STATE_FILES.state.to_list()
-
 
 # Question class that inherits from Turtle class
 class QuestionBoard(Turtle):
@@ -15,11 +12,14 @@ class QuestionBoard(Turtle):
         self.hideturtle()
         self.pu()
         self.score = 0
-        self.total_states = len(STATES_READ)
+        self.STATES_READ = STATE_FILES.state.to_list()
+        self.total_states = len(self.STATES_READ)
+
+
 
     def check_answer(self, state_answered):
         """returns True if the answered state is in the list of states"""
-        if state_answered in STATES_READ:
+        if state_answered in self.STATES_READ:
             return True
 
     def ask_question(self):
@@ -36,7 +36,7 @@ class QuestionBoard(Turtle):
         """:returns the x and y coordinates to be used to locate the state"""
         state_details = STATE_FILES[STATE_FILES.state == state_answered]
         position = (state_details.to_dict())
-        state_num = STATES_READ.index(state_answered)
+        state_num = self.STATES_READ.index(state_answered)
         position_x = position["x"][state_num]
         position_y = position["y"][state_num]
         return position_x, position_y
